75b4b3959599552d52a51dd31d5d32036b3561c6
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2018-02-10 23:42:23 +0100

    fix backend url

--- src/srv/slApi.service.ts
+++ src/srv/slApi.service.ts
@@ -12,7 +12,7 @@ export interface Departure {
 class SlApiService {
   async getDepartures (siteId: string, timeWindow: number = 30): Promise<any> {
     // const url = `http://api.sl.se/api2/realtimedeparturesV4.json?key=${secret.REALTIME_API_KEY}&siteid=${siteId}&timewindow=${timeWindow}&Bus=false&Train=false&Tram=false&Ship=false`
-    const url = `https://sl-dashboard-backend.now.sh/departures`
+    const url = `https://kg-backend.now.sh/sl/departures`
 
     const r = await fetch(url)
     return r.json()

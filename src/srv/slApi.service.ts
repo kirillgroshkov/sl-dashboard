7257745fed3199d1eb38cc59f47011de925d2e81
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2019-11-09 14:06:17 +0100

    fix: backend url

--- src/srv/slApi.service.ts
+++ src/srv/slApi.service.ts
@@ -11,8 +11,7 @@ export interface Departure {
 
 class SlApiService {
   async getDepartures (siteId: string, timeWindow: number = 30): Promise<any> {
-    // const url = `http://api.sl.se/api2/realtimedeparturesV4.json?key=${secret.REALTIME_API_KEY}&siteid=${siteId}&timewindow=${timeWindow}&Bus=false&Train=false&Tram=false&Ship=false`
-    const url = `https://kg-backend.now.sh/sl/departures`
+    const url = `https://kg-backend3.appspot.com/sl/departures`
 
     const r = await fetch(url)
     return r.json()
